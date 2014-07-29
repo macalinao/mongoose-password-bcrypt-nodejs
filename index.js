ec133cf695a04f4612d8173b947d010d872688ee
Ian Macalinao <me@ian.pw>
2014-07-29 08:43:05 -0500

    Add asynchronous password validation
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+var bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(schema, options) {
+  /**
+   * Checks if a password is valid.
+   *
+   * @param password - The password to check
+   * @param cb(err, res) - The callback, where `res` is a boolean.
+   */
+  schema.methods.validPassword = function(password, cb) {
+    return bcrypt.compare(password, this.password, cb);
+  };
+
   /**
    * Synchronously checks if a password is valid.
    *
