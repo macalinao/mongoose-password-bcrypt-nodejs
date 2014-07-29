79b8e516fda1f955666773c712172f2273903baf
Ian Macalinao <me@ian.pw>
2014-07-29 08:40:34 -0500

    Add validPasswordSync method
    Signed-off-by: Ian Macalinao <me@ian.pw>

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = function () {
-  return 'Hello, world';
+module.exports = function(schema, options) {
+  /**
+   * Synchronously checks if a password is valid.
+   *
+   * @param password The password to check
+   */
+  schema.methods.validPasswordSync = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
 };
